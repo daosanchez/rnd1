b793a7bfcb10eaa4a0337213affdbce99093375d
sanchez <145916188+ravzasanchez@users.noreply.github.com>
2023-10-24 14:51:30 +0300

    Add files via upload

---
+++ for-rnd/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace for_rnd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUret = new System.Windows.Forms.Button();
+            this.lbNotlar = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnUret
+            // 
+            this.btnUret.Font = new System.Drawing.Font("MV Boli", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUret.Location = new System.Drawing.Point(155, 12);
+            this.btnUret.Name = "btnUret";
+            this.btnUret.Size = new System.Drawing.Size(103, 46);
+            this.btnUret.TabIndex = 0;
+            this.btnUret.Text = "ÜRET";
+            this.btnUret.UseVisualStyleBackColor = true;
+            this.btnUret.Click += new System.EventHandler(this.btnUret_Click);
+            // 
+            // lbNotlar
+            // 
+            this.lbNotlar.Font = new System.Drawing.Font("MV Boli", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNotlar.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.lbNotlar.FormattingEnabled = true;
+            this.lbNotlar.ItemHeight = 17;
+            this.lbNotlar.Location = new System.Drawing.Point(12, 12);
+            this.lbNotlar.Name = "lbNotlar";
+            this.lbNotlar.Size = new System.Drawing.Size(120, 276);
+            this.lbNotlar.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(270, 319);
+            this.Controls.Add(this.lbNotlar);
+            this.Controls.Add(this.btnUret);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUret;
+        private System.Windows.Forms.ListBox lbNotlar;
+    }
+}
+
